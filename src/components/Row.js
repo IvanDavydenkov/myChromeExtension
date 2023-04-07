27a7fb4e1ef333933178deeced27b7bf3bb076c1
Ivan Davydenkov <krolik14121975@yandex.ru>
2023-04-07 19:31:44 +0300

    table is ready, chart is ready

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,17 +1,16 @@
 import React from "react";
-function normalizeTime(time){
+export function normalizeTime(time){
 	const newTime = new Date(time)
 	return `${newTime.getHours()}:${newTime.getMinutes()}`
 }
 const Row = (prop) => {
-	// console.log(prop)
-
+	console.log(prop)
 	return (
 		<li className='ex_list__item'>
 			<p className='ex_list__txt'>{`${normalizeTime(prop.start_time)}`}</p>
 			<p className='ex_list__txt'>{Math.floor(prop.sell_usd - prop.buy_usd)}</p>
-			<p className='ex_list__txt'>{prop.sell_usd}</p>
-			<p className='ex_list__txt'>{prop.buy_usd}</p>
+			<p className='ex_list__txt'>{Math.floor(prop.sell_usd)}</p>
+			<p className='ex_list__txt'>{Math.floor(prop.buy_usd)}</p>
 		</li>
 	);
 };
